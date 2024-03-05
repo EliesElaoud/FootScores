04bdd73ff5dea436b3cada87e8f440a100827077
EliesElaoud <elies.elaoud@gmail.com>
2024-03-05 12:28:21 +0100

    [ADD] informations screen

--- projects/application/src/app/pages/infos/infos.component.ts
+++ projects/application/src/app/pages/infos/infos.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CustomTitleComponent } from '../../components/custom-title/custom-title.component';
+import { CustomTextComponent } from '../../components/custom-text/custom-text.component';
 
 @Component({
   selector: 'app-infos',
   standalone: true,
-  imports: [],
+  imports: [CustomTitleComponent, CustomTextComponent],
   templateUrl: './infos.component.html',
   styleUrl: './infos.component.scss'
 })
