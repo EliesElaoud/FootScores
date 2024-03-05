770b8eed5b99b1d5829c559557ed08d31adf0ded
EliesElaoud <elies.elaoud@gmail.com>
2024-03-05 12:26:01 +0100

    [ADD] home screen

--- projects/application/src/app/pages/home/home.component.ts
+++ projects/application/src/app/pages/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CustomTitleComponent } from '../../components/custom-title/custom-title.component';
+import { CustomTextComponent } from '../../components/custom-text/custom-text.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CustomTitleComponent, CustomTextComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
